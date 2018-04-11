9ad6694b3d54172dec27d75b2c7d9ded60208a0d
Vivek Raj <raj.vivek.151297@gmail.com>
2018-04-11 21:25:59 +0530

    Back to the basics

--- app/src/main/java/com/software/vivek/notifierecosytem2/MainActivity.java
+++ app/src/main/java/com/software/vivek/notifierecosytem2/MainActivity.java
@@ -15,62 +15,14 @@ import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.software.vivek.notifierecosytem2.fragment.MyNotificationsFragment;
-import com.software.vivek.notifierecosytem2.fragment.MyPastRecords;
 
 public class  MainActivity extends BaseActivity {
-
-    private static final String TAG = "MainActivity";
-    private FragmentPagerAdapter mPagerAdapter;
-
-    private ViewPager mViewPager;
     private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-//        String key = mDatabase.child("notifications").push().getKey();
-//        Log.e("dsjkfjkl",key);
-        setContentView(R.layout.activity_main);
-
-        // Create the adapter that will return a fragment for each section
-        mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-            private final Fragment[] mFragments = new Fragment[] {
-                    new MyNotificationsFragment(),
-//                    new MyPastRecords()
-            };
-            private final String[] mFragmentNames = new String[] {
-                    getString(R.string.heading_pending),
-//                    getString(R.string.heading_all)
-            };
-            @Override
-            public Fragment getItem(int position) {
-                return mFragments[position];
-            }
-            @Override
-            public int getCount() {
-                return mFragments.length;
-            }
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return mFragmentNames[position];
-            }
-        };
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.container);
-        mViewPager.setAdapter(mPagerAdapter);
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        // Button launches NewPostActivity
-//        findViewById(R.id.fab_new_post).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                startActivity(new Intent(MainActivity.this, NewPostActivity.class));
-//            }
-//        });
-
     }
 
     @Override
